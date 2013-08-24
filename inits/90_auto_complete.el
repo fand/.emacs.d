ea102b97298f536990eebed3c4503358fe69de11
fand <s0811489@coins.tsukuba.ac.jp>
2013-08-24 21:32:50 +0900

    remove rubbish

--- inits/90_auto_complete.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20130724.1750/dict/")
-(require 'auto-complete-config)
-(ac-config-default)
-
-(global-auto-complete-mode t)
-;; 追加メジャーモード
-(add-to-list 'ac-modes 'org-mode)
-
