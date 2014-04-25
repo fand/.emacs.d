8eb980cb17ad23f4a83af2a859f875f927e96786
fand <fand>
2014-04-26 00:55:07 +0900

    quickrun

--- inits/03_popwin.el
+++ inits/03_popwin.el
@@ -1,4 +1,7 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
-
 (setq popwin:popup-window-position 'bottom)
+
+(require 'quickrun)
+(push '("*quickrun*") popwin:special-display-config)
+(global-set-key (kbd "<f5>") 'quickrun)
