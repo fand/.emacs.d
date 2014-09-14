75fdcf8307eba6f8d4b16a192a17d5e5a0ee5633
fand <s0811489@coins.tsukuba.ac.jp>
2014-09-15 02:49:23 +0900

    completion

--- inits/11_auto_complete.el
+++ inits/11_auto_complete.el
@@ -7,3 +7,17 @@
 ;; 追加メジャーモード
 (add-to-list 'ac-modes 'org-mode)
 
+(define-key ac-mode-map (kbd "C-\\") 'auto-complete)
+
+
+;; 7.5. 補完メニュー表示時のみC-n/C-pで補完候補を選択する
+(setq ac-use-menu-map t)
+;; デフォルトで設定済み
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+
+;; 大文字・小文字を区別しない
+(setq ac-ignore-case t)
+
+;; 20行分表示
+(setq ac-menu-height 20)
