ab96b69524b2aa073967638de0c77bdd0d5e0cb3
fand <s0811489@coins.tsukuba.ac.jp>
2014-09-15 02:49:39 +0900

    shibayu misc

--- inits/99_general.el
+++ inits/99_general.el
@@ -2,15 +2,24 @@
 
 
 ;; GNU global
-
-
 (when (locate-library "gtags")
-  (require 'gtags)
-  )
-
+  (require 'gtags))
 (global-set-key "\M-t" 'gtags-find-tag)     ;関数の定義元へ
 (global-set-key "\M-r" 'gtags-find-rtag)    ;関数の参照先へ
 (global-set-key "\M-s" 'gtags-find-symbol)  ;変数の定義元/参照先へ
 (global-set-key "\M-p" 'gtags-find-pattern)
 (global-set-key "\M-f" 'gtags-find-file)    ;ファイルにジャンプ
 (global-set-key [?\C-,] 'gtags-pop-stack)   ;前のバッファに戻る
+
+
+;; shibayu
+(setq completion-ignore-case t)
+(global-auto-revert-mode 1)
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+
+;; temp file directory
+(setq backup-directory-alist
+      (cons (cons ".*" (expand-file-name "~/.emacs.d/backup"))
+            backup-directory-alist))
